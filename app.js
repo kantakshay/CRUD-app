37f2e82e6d6e8830828fa86a7be59f1228695776
Akshay <akshaykant5006@gmail.com>
2023-12-24 07:23:23 +0530

    array data saved in localstorage and  map to post data

--- app.js
+++ app.js
@@ -1,48 +1,58 @@
-const form =  document.getElementById('form');
-const input = document.getElementById('input');
-const msg =  document.getElementById('msg')
-const post = document.getElementById('post');
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const input_disc = document.getElementById("input-disc");
+const msg = document.getElementById("msg");
+const post = document.getElementById("post");
 
-form.addEventListener('submit',(e)=>{
-e.preventDefault()
-console.log('btn clicked');
-validation()
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("btn clicked");
+  validation();
+});
 
-const validation = ()=>{
-  if(input.value === ""){
-    msg.innerHTML = "fill the task"
-  
-  }else{
-    msg.innerHTML = ""
+const validation = () => {
+  if (input.value === "" || input_disc.value === "") {
+    msg.innerHTML = "fill the task";
+  } else {
+    msg.innerHTML = "";
   }
-  addData()
-}
+  addData();
+};
 
-const data = {};
+const data = [];
 
-const addData=()=>{
-  data["text"] = input.value;
-  input.value = ""
+const addData = () => {
+  data.push({
+    tilte: input.value,
+    disc: input_disc.value,
+  }),
+    (input.value = "");
+  input_disc.value = "";
   console.log(data);
-  postData()
-}
+  localStorage.setItem("data",JSON.stringify(data))
+  postData();
+};
 
-const postData = () =>{
-  post.innerHTML += `<div>
-  <p>${data.text}</p>
-  <div>
-    <button onclick="deletePost(this)">Delete</button>
-    <button onclick="editPost(this)">Edit</button>
-  </div>
-</div>`
-}
+const postData = () => {
+  data.map((value) => {
+    return (post.innerHTML += `<div>
+       <p>${value.tilte}</p>
+    <p>${value.disc}</p>
+      <div>
+        <button onclick="deletePost(this)">Delete</button>
+        <button onclick="editPost(this)">Edit</button>
+      </div>
+    </div>`);
+  });
+};
 
-const deletePost = (e) =>{
+const deletePost = (e) => {
   e.parentElement.parentElement.remove();
-}
+};
 
-const editPost = (e) =>{
-input.value = e.parentElement.previousElementSibling.innerHTML;
-e.parentElement.parentElement.remove();
-}
+const editPost = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
+};
+
+
